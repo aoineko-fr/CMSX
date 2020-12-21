920020b4ebc8593655847e1cc5b83f3026b05432
Aoineko <aoineko@free.fr>
2020-12-22 00:53:05 +0100

    Fonts graphics update and moved inline print functions to .inl file

--- cmsx/data/builddata.bat
+++ cmsx/data/builddata.bat
@@ -1,16 +1,17 @@
-..\..\tools\MSXImage\MSXImage.exe -in font\misc\acme_5_outlines_font_gb.png -out font_acme.h -pos 0 0 -size 8 8 -num 16 4 -trans 0xFFFFFF -bpc 1 -name g_Font_Acme
-..\..\tools\MSXImage\MSXImage.exe -in font\font_darkrose.png -out font_darkrose.h    -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_Darkrose
-..\..\tools\MSXImage\MSXImage.exe -in font\IBMFont.bmp       -out font_ibm.h         -pos 0 0  -size 8 8  -gap 0 0 -num 16 16 -trans 0x040404 -bpc 1 -name g_Font_IBM
-..\..\tools\MSXImage\MSXImage.exe -in font\oxygene.png       -out font_oxygene.h     -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_Oxygene
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_1.png        -out font_cmsx_std1.h   -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std1
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_2.png        -out font_cmsx_std2.h   -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std2
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_3.png        -out font_cmsx_neon1.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Neon1
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_4.png        -out font_cmsx_mini1.h  -pos 0 16 -size 8 5  -gap 0 3 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Mini1
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_5.png        -out font_cmsx_neon2.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Neon2
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_6.png        -out font_cmsx_curs1.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Curs1
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_6_bold.png   -out font_cmsx_curs1b.h -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Curs1B
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_7.png        -out font_cmsx_std3.h   -pos 0 16 -size 8 7  -gap 0 1 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std3 
-..\..\tools\MSXImage\MSXImage.exe -in font\cmsx_9.png        -out font_cmsx_big1.h   -pos 0 0  -size 8 11 -gap 0 5 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Big1 
+REM ..\..\tools\MSXImage\MSXImage.exe -in font\misc\acme_5_outlines_font_gb.png -out font_acme.h -pos 0 0 -size 8 8 -num 16 4 -trans 0xFFFFFF -bpc 1 -name g_Font_Acme 					-skip -font 8 8 ! _
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_big1.png	-out font_cmsx_big1.h   -pos 0 0  -size 8 11 -gap 0 5 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Big1		-skip -font 8 11 ! _
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_curs1.png	-out font_cmsx_curs1.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Curs1		-skip -font 8 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_curs1b.png	-out font_cmsx_curs1b.h -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Curs1B	-skip -font 8 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_mini1.png	-out font_cmsx_mini1.h  -pos 0 16 -size 8 5  -gap 0 3 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Mini1		-skip -font 4 6 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_neon1.png	-out font_cmsx_neon1.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Neon1		-skip -font 8 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_neon1b.png	-out font_cmsx_neon1b.h	-pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Neon1B	-skip -font 8 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_neon2.png	-out font_cmsx_neon2.h  -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Neon2		-skip -font 8 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_std1.png	-out font_cmsx_std1.h   -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std1		-skip -font 6 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_std2.png	-out font_cmsx_std2.h   -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std2		-skip -font 6 8 ! ~
+..\..\tools\MSXImage\MSXImage.exe -in font\font_cmsx_std3.png	-out font_cmsx_std3.h   -pos 0 16 -size 8 7  -gap 0 1 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_CMSX_Std3		-skip -font 6 8 ! }
+REM ..\..\tools\MSXImage\MSXImage.exe -in font\font_darkrose.png	-out font_darkrose.h    -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_Darkrose		-skip -font 8 8 ! ~
+REM ..\..\tools\MSXImage\MSXImage.exe -in font\IBMFont.bmp			-out font_ibm.h         -pos 0 0  -size 8 8  -gap 0 0 -num 16 16 -trans 0x040404 -bpc 1 -name g_Font_IBM			-skip -font 8 8 0x01 0xFE
+REM ..\..\tools\MSXImage\MSXImage.exe -in font\oxygene.png			-out font_oxygene.h     -pos 0 16 -size 8 8  -gap 0 0 -num 16 6  -trans 0x000000 -bpc 1 -name g_Font_Oxygene		-skip -font 8 8 ! _
 
 
 PAUSE
--- cmsx/src/print.h
+++ cmsx/src/print.h
@@ -59,79 +59,17 @@ void Print_SetShadow(bool activate, i8 offsetX, i8 offsetY, u8 color);
 #endif
 
 //-----------------------------------------------------------------------------
-/// Set the current font
-inline void Print_SetFontEx(u8 formX, u8 formY, u8 sizeX, u8 sizeY, u8 firstChr, u8 lastChr, const u8* forms)
-{
-	g_PrintData.FormX     = formX;
-	g_PrintData.FormY     = formY;
-	g_PrintData.UnitX     = sizeX;
-	g_PrintData.UnitY     = sizeY;
-	g_PrintData.FontFirst = firstChr;
-	g_PrintData.FontLast  = lastChr;
-	g_PrintData.FontForms = forms;
-}
-
+// INLINE FUNCTIONS
 //-----------------------------------------------------------------------------
-/// Set the draw color
-inline void Print_SetColor(u8 text, u8 background)
-{
-	g_PrintData.TextColor = text;
-	g_PrintData.BackgroundColor = background;
-
-	if((g_PrintData.Mode == VDP_MODE_GRAPHIC4) || (g_PrintData.Mode == VDP_MODE_GRAPHIC6))
-	{
-		g_PrintData.Buffer[0] = g_PrintData.BackgroundColor << 4 | g_PrintData.BackgroundColor;
-		g_PrintData.Buffer[1] = g_PrintData.BackgroundColor << 4 | g_PrintData.TextColor;
-		g_PrintData.Buffer[2] = g_PrintData.TextColor << 4 | g_PrintData.BackgroundColor;
-		g_PrintData.Buffer[3] = g_PrintData.TextColor << 4 | g_PrintData.TextColor;
-	}
-}
-
-//-----------------------------------------------------------------------------
-/// Set cursor position
-inline void Print_SetPosition(u8 x, u8 y)
-{
-	g_PrintData.CursorX = x;
-	g_PrintData.CursorY = y;	
-}
-
-//-----------------------------------------------------------------------------
-/// Set cursor position
-inline void Print_SetCharSize(u8 x, u8 y)
-{
-	g_PrintData.UnitX = x;
-	g_PrintData.UnitY = y;	
-}
-
-//-----------------------------------------------------------------------------
-/// Set tabulation size in pixel (must be a power of 2 like 16, 32, 64, ...)
-inline void Print_SetTabSize(u8 size)
-{
-	g_PrintData.TabSize = size;
-}
-
-//-----------------------------------------------------------------------------
-/// Print space
-inline void Print_Space()
-{
-	g_PrintData.CursorX += g_PrintData.UnitX;
-}
-
-//-----------------------------------------------------------------------------
-/// Print tabulation
-inline void Print_Tab()
-{
-	g_PrintData.CursorX += g_PrintData.UnitX + g_PrintData.TabSize - 1;
-	g_PrintData.CursorX &= ~(g_PrintData.TabSize - 1);
-}
-
-//-----------------------------------------------------------------------------
-/// Print return
-inline void Print_Return()
-{
-	g_PrintData.CursorX = 0;
-	g_PrintData.CursorY += g_PrintData.UnitY;
-}
+// inline void Print_SetFontEx(u8 formX, u8 formY, u8 sizeX, u8 sizeY, u8 firstChr, u8 lastChr, const u8* forms); // Set the current font
+// inline void Print_SetColor(u8 text, u8 background); // Set the draw color
+// inline void Print_SetPosition(u8 x, u8 y); // Set cursor position
+// inline void Print_SetCharSize(u8 x, u8 y); // Set cursor position
+// inline void Print_SetTabSize(u8 size) // Set tabulation size in pixel (must be a power of 2 like 16, 32, 64, ...);
+// inline void Print_Space(); // Print space
+// inline void Print_Tab(); // Print tabulation
+// inline void Print_Return(); // Print return
+#include "print.inl"
 
 //-----------------------------------------------------------------------------
 // DRAW FUNCTION
